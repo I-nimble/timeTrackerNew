5137f951a62d160715efcb5d4277585e2707fe27
Sofia Bracho <sofia_bracho1@hotmail.com>
2025-05-09 15:02:44 -0400

    Changed productivity report component name

--- src/app/pages/dashboards/reports/reports.component.ts
+++ src/app/pages/dashboards/reports/reports.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { AppTrafficDistributionComponent } from '../../../components/dashboard2/traffic-distribution/traffic-distribution.component';
+import { AppActivityReportComponent } from '../../../components/dashboard2/activity-report/activity-report.component';
 import { AppEmployeesReportsComponent } from '../../../components/dashboard2/employees-reports/employees-reports.component';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [AppTrafficDistributionComponent, AppEmployeesReportsComponent],
+  imports: [AppActivityReportComponent, AppEmployeesReportsComponent],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
