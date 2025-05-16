5f0111af95f0f69b227be5fffdc08fede1964308
Naisireth <npena@i-nimble.com>
2025-05-16 16:21:02 -0400

    SD-384: Funcionalidad de kanban

--- src/app/pages/apps/kanban/kanban-dialog.component.ts
+++ src/app/pages/apps/kanban/kanban-dialog.component.ts
@@ -5,38 +5,54 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { MaterialModule } from 'src/app/material.module';
 import { DatePipe } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { RatingsService } from 'src/app/services/ratings.service';
 @Component({
     selector: 'app-kanban-dialog',
     templateUrl: './kanban-dialog.component.html',
+    standalone: true,
     imports: [
         MaterialModule,
         CommonModule,
         TablerIconsModule,
         FormsModule,
         ReactiveFormsModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ],
-    providers: [DatePipe]
+    providers: [DatePipe, provideNativeDateAdapter()]
 })
 export class AppKanbanDialogComponent {
   action: string;
   local_data: any;
+  priorities: any[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<AppKanbanDialogComponent>,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any,
-    private datePipe: DatePipe
+    private datePipe: DatePipe,
+    public ratingsService: RatingsService
   ) {
+    this.getPriorities();
     this.local_data = { ...data };
-
-    if (data.action === 'Add') {
-      this.local_data.date = this.datePipe.transform(new Date(), 'd MMMM')!;
-      this.local_data.taskProperty = 'Design';
-      this.local_data.imageUrl = '/assets/images/taskboard/kanban-img-1.jpg';
-    } else if (data.action === 'Edit') {
-      this.local_data.imageUrl = data.imageUrl;
-    }
-
     this.action = this.local_data.action;
+    
+    if (data.type === 'board') {
+      this.local_data.type = 'board';
+    } else {
+      this.local_data.type = 'task';
+      this.local_data.date = this.datePipe.transform(new Date(), 'd MMMM')!;
+      this.local_data.taskProperty ||= 'Design';
+      this.local_data.imageUrl ||= '/assets/images/taskboard/kanban-img-1.jpg';
+    }
+  }
+
+  getPriorities() {
+    this.ratingsService.getPriorities().subscribe((priorities: any[]) => {
+      this.priorities = priorities;
+      console.log(this.priorities)
+    });
   }
 
   doAction(): void {
