e77580885da4f29cf8987d8a88fd310b74388b80
Sofia Bracho <sofia_bracho1@hotmail.com>
2025-05-16 10:35:50 -0400

    intake: Solicitud de Talento

---
+++ src/app/services/intake.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IntakeService {
+  constructor(private http: HttpClient) {}
+  private API_URI = environment.apiUrl + '/intake';
+
+  public submit(data: any) {
+    const body = {
+      company_name: data.companyName,
+      contact_name: data.name,
+      email: data.email,
+      phone: data.countryCode + ' ' + data.phone,
+      job_name_description: data.jobNameAndDescription,
+      required_skills: data.requiredSkills.join(', '),
+      routine_oriented: data.routineOriented,
+      social_oriented: data.socialOriented,
+      decision_making: data.decisionMaking,
+      attention_to_detail: data.attentionToDetail,
+      management_style: data.managementStyle,
+      feedback_style: data.feedbackStyle,
+      communication_style: data.communicationStyle,
+      conflict_handling: data.conflictHandling
+    }
+
+    return this.http.post(`${this.API_URI}`, body);
+  }
+}
