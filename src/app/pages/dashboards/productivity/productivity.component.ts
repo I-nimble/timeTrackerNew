d0595b2aa3a73071af6fc4109e8fb72555ae7db1
unknown <sssmonsalve@gmail.com>
2025-06-12 09:48:25 -0400

    Merged with SD-448 reports

--- src/app/pages/dashboards/productivity/productivity.component.ts
+++ src/app/pages/dashboards/productivity/productivity.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { AppDailyProductivityComponent } from 'src/app/components/dashboard2/daily-productivity/daily-productivity.component';
-import { AppMixTableComponent } from '../../apps/storage/mix-table/mix-table.component';
+import { AppActivityReportsComponent } from '../../../components/dashboard2/app-activity-reports/activity-reports.component';
+import { AppProductivityReportsComponent } from 'src/app/components/dashboard2/productivity-reports/productivity-reports.component';
 
 
 @Component({
   selector: 'app-productivity',
   standalone: true,
-  imports: [AppDailyProductivityComponent, AppMixTableComponent],
+  imports: [AppDailyProductivityComponent, AppActivityReportsComponent, AppProductivityReportsComponent],
   templateUrl: './productivity.component.html',
   styleUrl: './productivity.component.scss'
 })
 export class ProductivityComponent {
+  dataSource: any[] = [];
 
+  onEmployeesDataSourceChange(data: any[]) {
+      this.dataSource = data;
+  }
 }
