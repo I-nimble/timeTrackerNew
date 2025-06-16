63113601eb9916da96cd081a8055ccd6eb76f819
unknown <sssmonsalve@gmail.com>
2025-06-16 13:18:59 -0400

    requested changes

--- src/app/pages/talent-match-admin/talent-match-admin.component.ts
+++ src/app/pages/talent-match-admin/talent-match-admin.component.ts
@@ -76,35 +76,10 @@ export class AppTalentMatchAdminComponent implements OnInit {
     private snackBar: MatSnackBar
   ) {}
 
-  // /** Whether the number of selected elements matches the total number of rows. */
-  // isAllSelected(): any {
-  //   const numSelected = this.selection.selected.length;
-  //   const numRows = this.dataSource.data.length;
-  //   return numSelected === numRows;
-  // }
-
-  // /** Selects all rows if they are not all selected; otherwise clear selection. */
-  // masterToggle(): void {
-  //   this.isAllSelected()
-  //     ? this.selection.clear()
-  //     : this.dataSource.data.forEach((row) => this.selection.select(row));
-  // }
-
-  // /** The label for the checkbox on the passed row */
-  // checkboxLabel(row?: PeriodicElement): string {
-  //   if (!row) {
-  //     return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-  //   }
-  //   return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
-  //     row.position + 1
-  //   }`;
-  // }
-
   getApplications() {
     this.applicationService.get().subscribe((applications) => {
       this.applicationsData = applications;
       this.dataSource.data = applications;
-      console.log(applications)
     });
     this.companiesService.getCompanies().subscribe({
       next: companies => {
