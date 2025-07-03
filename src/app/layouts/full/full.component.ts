c4be64ca7fed97d9d035bd9730f0a14d7d9e202c
Sofia Bracho <sofia_bracho1@hotmail.com>
2025-07-03 12:36:58 -0400

    feat: SD-581 responsive navbar

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -116,103 +116,103 @@ export class FullComponent implements OnInit {
     {
       id: 1,
       img: '/assets/images/svgs/icon-dd-chat.svg',
-      title: 'Chat Application',
-      subtitle: 'Messages & Emails',
+      title: 'Chat',
+      subtitle: 'Internal communication',
       link: '/apps/chat',
     },
-    {
-      id: 2,
-      img: '/assets/images/svgs/icon-dd-cart.svg',
-      title: 'eCommerce App',
-      subtitle: 'Buy a Product',
-      link: '/apps/email/inbox',
-    },
-    {
-      id: 3,
-      img: '/assets/images/svgs/icon-dd-invoice.svg',
-      title: 'Invoice App',
-      subtitle: 'Get latest invoice',
-      link: '/apps/invoice',
-    },
+    // {
+    //   id: 2,
+    //   img: '/assets/images/svgs/icon-dd-cart.svg',
+    //   title: 'eCommerce App',
+    //   subtitle: 'Buy a Product',
+    //   link: '/apps/email/inbox',
+    // },
+    // {
+    //   id: 3,
+    //   img: '/assets/images/svgs/icon-dd-invoice.svg',
+    //   title: 'Invoice App',
+    //   subtitle: 'Get latest invoice',
+    //   link: '/apps/invoice',
+    // },
     {
       id: 4,
       img: '/assets/images/svgs/icon-dd-date.svg',
-      title: 'Calendar App',
-      subtitle: 'Get Dates',
+      title: 'Calendar',
+      subtitle: 'Manage tasks by date',
       link: '/apps/calendar',
     },
-    {
-      id: 5,
-      img: '/assets/images/svgs/icon-dd-mobile.svg',
-      title: 'Contact Application',
-      subtitle: '2 Unsaved Contacts',
-      link: '/apps/contacts',
-    },
+    // {
+    //   id: 5,
+    //   img: '/assets/images/svgs/icon-dd-mobile.svg',
+    //   title: 'Contact Application',
+    //   subtitle: '2 Unsaved Contacts',
+    //   link: '/apps/contacts',
+    // },
     {
       id: 6,
       img: '/assets/images/svgs/icon-dd-lifebuoy.svg',
-      title: 'Tickets App',
-      subtitle: 'Create new ticket',
-      link: '/apps/tickets',
+      title: 'Kanban',
+      subtitle: 'Create tickets',
+      link: '/apps/kanban',
     },
     {
       id: 7,
-      img: '/assets/images/svgs/icon-dd-message-box.svg',
-      title: 'Email App',
-      subtitle: 'Get new emails',
-      link: '/apps/email/inbox',
+      img: '/assets/images/svgs/icon-dd-application.svg',
+      title: 'Time tracker',
+      subtitle: 'Track your team time',
+      link: 'apps/time-tracker',
     },
     {
       id: 8,
-      img: '/assets/images/svgs/icon-dd-application.svg',
-      title: 'Courses',
-      subtitle: 'Create new course',
-      link: '/apps/courses',
+      img: '/assets/images/svgs/icon-dd-invoice.svg',
+      title: 'Notes',
+      subtitle: 'Keep personal notes',
+      link: '/apps/notes',
     },
   ];
 
-  quicklinks: quicklinks[] = [
-    {
-      id: 1,
-      title: 'Pricing Page',
-      link: '/theme-pages/pricing',
-    },
-    {
-      id: 2,
-      title: 'Authentication Design',
-      link: '/authentication/login',
-    },
-    {
-      id: 3,
-      title: 'Register Now',
-      link: '/authentication/side-register',
-    },
-    {
-      id: 4,
-      title: '404 Error Page',
-      link: '/authentication/error',
-    },
-    {
-      id: 5,
-      title: 'Notes App',
-      link: '/apps/notes',
-    },
-    {
-      id: 6,
-      title: 'Time tracker',
-      link: '/apps/employee',
-    },
-    {
-      id: 7,
-      title: 'Todo Application',
-      link: '/apps/todo',
-    },
-    {
-      id: 8,
-      title: 'Treeview',
-      link: '/theme-pages/treeview',
-    },
-  ];
+  // quicklinks: quicklinks[] = [
+  //   {
+  //     id: 1,
+  //     title: 'Pricing Page',
+  //     link: '/theme-pages/pricing',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Authentication Design',
+  //     link: '/authentication/login',
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Register Now',
+  //     link: '/authentication/side-register',
+  //   },
+  //   {
+  //     id: 4,
+  //     title: '404 Error Page',
+  //     link: '/authentication/error',
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'Notes App',
+  //     link: '/apps/notes',
+  //   },
+  //   {
+  //     id: 6,
+  //     title: 'Time tracker',
+  //     link: '/apps/employee',
+  //   },
+  //   {
+  //     id: 7,
+  //     title: 'Todo Application',
+  //     link: '/apps/todo',
+  //   },
+  //   {
+  //     id: 8,
+  //     title: 'Treeview',
+  //     link: '/theme-pages/treeview',
+  //   },
+  // ];
 
   constructor(
     private settings: CoreService,
