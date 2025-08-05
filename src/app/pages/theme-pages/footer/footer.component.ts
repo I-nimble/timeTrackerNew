af887aeb04dec2bc0c5d6f731fbd519935a15785
Sofia Bracho <sofia_bracho1@hotmail.com>
2025-08-05 16:59:02 -0400

    feat: SD-669 landing page redesign

--- src/app/pages/theme-pages/footer/footer.component.ts
+++ src/app/pages/theme-pages/footer/footer.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BrandingComponent } from 'src/app/layouts/full/vertical/sidebar/branding.component';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [BrandingComponent,TablerIconsModule,RouterLink,],
+  imports: [BrandingComponent,TablerIconsModule,RouterLink,ButtonComponent],
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 
 export class AppFooterComponent {
