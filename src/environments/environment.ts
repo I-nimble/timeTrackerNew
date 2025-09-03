504e1a7edf713fd10ff5d5c25fef018f6c0a3dae
andreavds <andreadiazsoto@hotmail.com>
2025-09-03 14:26:58 -0400

    fix: SD-798 spinner position

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,5 +18,5 @@ export const environment = {
     appId: "1:562152489018:web:10df1fd86381626629f503"
   },
   vapidKey: 'BAgM65bd3u0eip66PuUViB9xB4agoBNFqTOragNBofHJnz7BcZM4SJqoSZyu2gj8Aiz573ExvVDwfU_eICWf2JI',
-  allowedReportEmails: ['pgarcia@i-nimble.com', 'jnava@i-nimble.com'],
+  allowedReportEmails: ['pgarcia@i-nimble.com', 'jnava@i-nimble.com', 'sgutierrez@i-nimble.com'],
 };
