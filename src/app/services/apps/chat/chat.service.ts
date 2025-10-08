cc1443bbd64ee16a19a1d7ed4cee2ff1ca8e5253
Sofia Bracho <sofia_bracho1@hotmail.com>
2025-10-08 11:38:13 -0400

    fix: removed setContactsConfiguration method call on chat service

--- src/app/services/apps/chat/chat.service.ts
+++ src/app/services/apps/chat/chat.service.ts
@@ -42,7 +42,6 @@ export class CometChatService {
       const loggedIn = await this.loginCometChatUser(chat_uid);
       if (!loggedIn) return;
 
-
       this.isChatAvailable = true;
 
       const permissionGranted = await this.requestNotificationPermission();
