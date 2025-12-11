93c1f8c3e47607879dd1c7185152ffc1f96708d5
andreavds <andreadiazsoto@hotmail.com>
2025-12-11 10:15:40 -0400

    feat: SD-1185 events section

--- src/app/layouts/full/vertical/header/header.component.ts
+++ src/app/layouts/full/vertical/header/header.component.ts
@@ -269,14 +269,6 @@ export class HeaderComponent implements OnInit {
           subtitle: 'Document your future plans',
           link: 'apps/r3',
         },
-        {
-          id: 6,
-          img: 'shield',
-          color: 'success',
-          title: 'Events',
-          subtitle: 'Events',
-          link: 'apps/events',
-        },
       ];
     }
 
