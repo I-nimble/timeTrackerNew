3adb27e1a5e85b5c05e0381c2cc6d5e58a9b4a0b
Sofia Bracho <sbrachodev@gmail.com>
2026-02-19 19:06:59 -0400

    feat: add ThemeService for dynamic theme management and update R3 component to use it

--- src/app/pages/apps/r3/r3.component.ts
+++ src/app/pages/apps/r3/r3.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material.module';
 import { MatchComponent } from 'src/app/components/match-search/match.component';
 import { MarkdownPipe, LinebreakPipe } from 'src/app/pipe/markdown.pipe';
-import { RouterLink, RouterModule } from '@angular/router';
-import { ChangeDetectorRef } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-r3',
@@ -20,41 +20,6 @@ import { ChangeDetectorRef } from '@angular/core';
     RouterModule,
   ]
 })
-export class R3Component implements OnInit, OnDestroy {
-  theme!: string;
-  htmlElement!: HTMLElement;
-  private themeObserver: MutationObserver | null = null;
-
-  constructor(
-    private cdr: ChangeDetectorRef
-  ) {}
-
-  ngOnInit(): void {
-    this.htmlElement = document.querySelector('html')!;
-    this.checkTheme();
-    
-    this.themeObserver = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-          this.checkTheme();
-        }
-      });
-    });
-
-    this.themeObserver.observe(this.htmlElement, {
-      attributes: true,
-      attributeFilter: ['class'],
-    });
-  }
-
-  checkTheme(): void {
-    this.theme = this.htmlElement.classList.contains('dark-theme') ? 'dark' : 'light';
-    this.cdr.detectChanges();
-  }
-
-  ngOnDestroy(): void {
-    if (this.themeObserver) {
-      this.themeObserver.disconnect();
-    }
-  }
+export class R3Component {
+  constructor(public themeService: ThemeService) {}
 }
