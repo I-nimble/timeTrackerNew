467124b2b0ecb52205878df1b87e7477ea3f2137
andreavds <andreadiazsoto@hotmail.com>
2026-02-19 11:57:26 -0400

    fix: SD-1395 work experience summary console error

--- src/app/pages/candidates/candidate-details/candidate-details.component.ts
+++ src/app/pages/candidates/candidate-details/candidate-details.component.ts
@@ -120,7 +120,7 @@ export class CandidateDetailsComponent implements OnInit {
       interview_link: [''],
       hobbies: [''],
       work_experience: ['', Validators.maxLength(1000)],
-      work_experience_summary: ['', Validators.required, Validators.maxLength(200)],
+      work_experience_summary: ['', [Validators.required, Validators.maxLength(200)]],
       skills: ['', Validators.required],
       education_history: [''],
       inimble_academy: [''],
