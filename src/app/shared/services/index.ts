d10327243f999f8857dd0dd79d36e32842737b8f
Sofia Bracho <sbrachodev@gmail.com>
2026-04-24 12:55:19 -0400

    feat(index): SD-2200 create index file to export shared services and types

---
+++ src/app/shared/services/index.ts
@@ -0,0 +1,11 @@
+export { LoggerService, type LogLevel } from './logger.service';
+export {
+  NotificationService,
+  type NotificationOptions,
+  type NotificationType,
+} from './notification.service';
+export {
+  ErrorHandlerService,
+  provideGlobalErrorHandler,
+  type ErrorHandlerOptions,
+} from './error-handler.service';
